0ae554efb8be9db028fe2a472430b8e0b897e87d
iwarei <jjjjjjjjjjjjjjjjj93@gmail.com>
2023-08-12 19:47:17 +0900

    [FIX] リファクタリング

---
+++ resources/views/components/review-rating.blade.php
@@ -0,0 +1,28 @@
+@props(['review'])
+
+<div class="flex items-center mt-2.5 mb-5">
+  @for ($i = 0; $i < 5; $i++)
+    @if ($review >= 1.0)
+      <svg class="w-4 h-4 text-yellow-300 mr-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+        <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"/>
+      </svg>
+    @elseif($review <= 1.0 && $review > 0.0)
+      <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 22 20">
+        <defs>
+          <linearGradient id="halfYellowHalfGray" x1="0%" y1="0%" x2="100%" y2="0%">
+            <stop offset="{{ $review * 100 }}%" style="stop-color: #faca15; stop-opacity: 1" />
+            <stop offset="{{ $review * 100 }}%" style="stop-color: #d5d7db; stop-opacity: 1" />
+          </linearGradient>
+        </defs>
+        <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" fill="url(#halfYellowHalfGray)" />
+      </svg>
+    @else
+      <svg class="w-4 h-4 text-gray-200" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+        <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"/>
+      </svg>
+    @endif
+    @php
+      $review -= 1.0
+    @endphp
+  @endfor
+</div>
