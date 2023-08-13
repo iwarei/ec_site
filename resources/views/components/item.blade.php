10931307d89b43a75ee8ede6e21c5e3e34e027a2
iwarei <jjjjjjjjjjjjjjjjj93@gmail.com>
2023-08-13 14:41:27 +0900

    [UPDATE] トップページ作成

--- resources/views/components/item.blade.php
+++ resources/views/components/item.blade.php
@@ -1,16 +1,14 @@
 @props(['item', 'showSimple' => false])
 
-@if ($showSimple) 
-
-@else
-  <div class="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+<div class="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+  <a href="#">
+    <img src="{{ $item->topImage() }}">
+  </a>
+  <div class="px-3 pb-4">
     <a href="#">
-      <img src="{{ $item->topImage() }}">
+      <h5 class="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">{{ $item->name }}</h5>
     </a>
-    <div class="px-3 pb-4">
-      <a href="#">
-        <h5 class="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">{{ $item->name }}</h5>
-      </a>
+    @if (!$showSimple)
       {{-- レビュー評価 --}}
       <x-review-rating :item="$item" showModal="true" showRate="true" />
       {{-- 金額表示 --}}
@@ -28,8 +26,8 @@
       </div>
       {{-- カートに入れるボタン --}}
       <div class="flex items-center justify-between mt-2">
-        <a href="#" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-bluedark:focus:ring-blue-800">カートに入れる</a>
+        <a href="#" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2.5 text-center dark:bg-blue-600   dark:hover:bg-bluedark:focus:ring-blue-800">カートに入れる</a>
       </div>
-    </div>
+    @endif
   </div>
-@endif
+</div>
