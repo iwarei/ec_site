9f34134d449fa6cf727dbfe2397db6eb35ec8b5b
iwarei <jjjjjjjjjjjjjjjjj93@gmail.com>
2023-08-25 23:54:12 +0900

    [UPDATE] 商品詳細画面: 作成中...

--- resources/views/user/item/show.blade.php
+++ resources/views/user/item/show.blade.php
@@ -225,9 +225,11 @@
           {{-- ToDo: レビュー表示部品化 --}}
           <article>
             <div class="flex items-start mb-1 space-x-4">
-              <img class="w-10 h-10 rounded-full" src="https://flowbite.com/docs/images/people/profile-picture-5.jpg" alt="">
+              <img  class="w-10 h-10 rounded-full" src="{{ Auth::user()?->reviewer()?->iconSrc ?? asset('image/default_icon.jpg') }}" alt="">
               <div class="space-y-1 font-medium">
-                <p class="mb-1">Jese Leos <time datetime="2014-08-16 19:00" class="block text-sm text-gray-500">Joined on August 2014</time></p>
+                <p class="mb-1">{{ Auth::user()?->reviewer()?->name ?? 'ななしさん' }}
+                  <time datetime="2014-08-16 19:00" class="block text-sm text-gray-500">レビュー登録日: </time>
+                </p>
               </div>
             </div>
             <div class="flex items-start mb-1">
